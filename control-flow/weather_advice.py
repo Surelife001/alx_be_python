623dd809a1e5632c69fbc7552de5ab89b8332724
Favour Ubah <favourubah092@gmail.com>
2025-11-17 03:23:04 +0100

    let try this

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -3,10 +3,11 @@
 user_input = input("What's the weather like today? (sunny/rainy/cold):").lower()
 # control flow to provide advice based on weather
 if user_input is "sunny":
-    print("Wear a t-shirt and sunglasses.")
+    recommendation = "Wear a t-shirt and sunglasses."
 elif user_input is "rainy":
-    print("Don't forget your umbrella and a raincoat.")
+    recommendation ="Don't forget your umbrella and a raincoat."
 elif user_input is "cold":
-    print(" Make sure to wear a warm coat and a scarf.")
+    recommendation =" Make sure to wear a warm coat and a scarf."
 else:
-    print(" Sorry, I don't have recommendations for this weather.")
+    recommendation="Sorry, I don't have recommendations for this weather."
+print(recommendation)
