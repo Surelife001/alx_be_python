a7eb7358c41869acf592548f347ac244a1147b6c
Favour Ubah <favourubah092@gmail.com>
2025-11-17 03:08:08 +0100

    correct of line of code

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,12 +1,12 @@
 # user input
 
-user_input = input("What's the weather like today? (sunny/rainy/cold):")
+user_input = input("What's the weather like today? (sunny/rainy/cold):").lower()
 # control flow to provide advice based on weather
-if user_input.lower() == "sunny":
+if user_input == "sunny":
     print("Wear a t-shirt and sunglasses.")
-elif user_input.lower() == "rainy":
+elif user_input == "rainy":
     print("Don't forget your umbrella and a raincoat.")
-elif user_input.lower() == "cold":
+elif user_input == "cold":
     print(" Make sure to wear a warm coat and a scarf.")
 else:
     print(" Sorry, I don't have recommendations for this weather.")
