b936ace1b8522bb213c50560f691bf398137c4a7
Favour Ubah <favourubah092@gmail.com>
2025-11-23 23:07:14 +0100

    new work

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,25 @@
+def perform_operation(num1, num2, operation):
+    """
+    Performs basic arithmetic operations.
+    :param num1: float
+    :param num2: float
+    :param operation: string ('add', 'subtract', 'multiply', 'divide')
+    :return: result of the arithmetic operation or an error message
+    """
+
+    if operation == "add":
+        return num1 + num2
+
+    elif operation == "subtract":
+        return num1 - num2
+
+    elif operation == "multiply":
+        return num1 * num2
+
+    elif operation == "divide":
+        if num2 == 0:
+            return "Error: Division by zero is not allowed."
+        return num1 / num2
+
+    else:
+        return "Error: Invalid operation."
